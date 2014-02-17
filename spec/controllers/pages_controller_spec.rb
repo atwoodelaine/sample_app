82b183c7dda228b04157a2ee6c4b564fa63359f0
Elaine Atwood <becky.e.atwood@gmail.com>
2014-02-17 19:28:04 +0100

    Adding stylesheet

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -16,7 +16,7 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-                                    :content => "#{@base_title" | Home)
+                                    :content => "#{@base_title} | Home")
     end
 
     it "should have a non-black body" do
@@ -33,7 +33,7 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-                                    :content => "#{@base_title" | Contact")
+                                    :content => "#{@base_title} | Contact")
     end
     it "should have a non-black body" do
       get 'contact'
@@ -49,7 +49,7 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_selector("title",
-                                   :content => "#{@base_title" | About")
+                                   :content => "#{@base_title} | About")
     end
     it "should have a non-black body" do
       get 'about'
