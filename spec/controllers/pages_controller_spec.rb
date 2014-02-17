007be4a637f11dc23f06f4b8fa571cd0dc54f125
Elaine Atwood <becky.e.atwood@gmail.com>
2014-02-17 17:21:34 +0100

    Completed layout structure

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,16 +1,22 @@
 require 'spec_helper'
 
 describe PagesController do
-render_views
+  render_views
+
+  before(:each)do 
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end 
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
+    end 
+
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-                    :content =>"Ruby on Rails Tutorials Sample App | Home")
+                                    :content => "#{@base_title" | Home)
     end
 
     it "should have a non-black body" do
@@ -27,7 +33,11 @@ render_views
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-                    :content =>"Ruby on Rails Tutorials Sample App | Contact")
+                                    :content => "#{@base_title" | Contact")
+    end
+    it "should have a non-black body" do
+      get 'contact'
+      response.body.should_not =~ /<body>\s*<\/body>/
     end
   end
 
@@ -39,8 +49,11 @@ render_views
     it "should have the right title" do
       get 'about'
       response.should have_selector("title",
-                    :content =>"Ruby on Rails Tutorials Sample App | About")
+                                   :content => "#{@base_title" | About")
+    end
+    it "should have a non-black body" do
+      get 'about'
+      response.body.should_not =~ /<body>\s*<\/body>/
     end
   end  
-
 end
